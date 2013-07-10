c544f8d967c2d53b08b48f7394a0b0e5f5d58923
abhishek luv <abhishek.luv@outlook.com>
2013-07-10 06:50:23 +0000

    Web.js prints content of index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ // response.send('Hello World 2!');
+    fs.readFile('index.html',function(err,data){
+     if(err) throw err;
+      response.send(data.toString());
+});
 });
 
 var port = process.env.PORT || 5000;
